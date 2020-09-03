5c3551224799fcb7863e822d9e0316f0c6a878e5
armandoMateu <jejeje_mateu@hotmail.com>
2020-09-03 19:41:15 -0400

    Update FernetDecrypt.py

--- FernetDecrypt.py
+++ FernetDecrypt.py
@@ -2,7 +2,7 @@
 from cryptography.fernet import Fernet as frt
 
 key="" #Add key
-token=""Add toke
+token="" #Add toke
 
 print('key: {0}, type: {1}'.format(key, type(key)))
 print('token: {0}, type: {1}'.format(token, type(token)))
